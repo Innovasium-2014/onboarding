3060b8b5ae41ace1e20fa9b40a1e5e5f10e3585e
James Hageman <jamesd@hageman.ca>
2016-08-31 08:40:12 -0400

    add redux logger

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
 
 import App from './components/App';
 
+const logger = createLogger();
+
 const store = createStore(rootReducer, {}, compose(
-  applyMiddleware(thunk),
+  applyMiddleware(thunk, logger),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
