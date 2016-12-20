71b06b3e78eb1e0d1122dede3b5d10243ea5f52d
Jordan Edwards <williamlo@Tormund.local>
2016-12-20 17:16:26 -0500

    removed file content for tutorial starting point

--- src/actions/StudentActions.js
+++ src/actions/StudentActions.js
@@ -1,8 +1,1 @@
-import { STUDENTS_LOAD } from '../constants/ActionTypes';
 
-export const students = {
-  // TODO use thunk to fetch the students
-  load: () => ({
-    type: STUDENTS_LOAD
-  })
-};
